7f7046e0d0e747e617c88942329324fee22bf371
MUmairqureshi <umairqadeer586@gmail.com>
2022-06-18 12:50:05 -1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-// import {card} from './components/cart/card'
+
 import {Header } from './components/header/header'
-// import {Navbar} from './components/header/navbar'
-// import {Heade} from './components'
-// import {Card} from './components/cart/card'
 
 
 function App() {
